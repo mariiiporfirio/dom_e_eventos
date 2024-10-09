f4e096166e8ac437999dbb91498937b5bef1fd2d
myllennah <milena.rmota@senacsp.edu.br>
2024-10-08 22:22:23 -0300

    Update script_dom_eventos.js

--- bolo_leila-main/js/script_dom_eventos.js
+++ bolo_leila-main/js/script_dom_eventos.js
@@ -39,14 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Exercício 4
-const link = document.getElementById("item");
-const mostrarDescricao = document.getElementsByClassName("descricao")[0];
- 
-    link.addEventListener("click", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const descricao = document.getElementsByClassName("descricao")[0];
+    
+    descricao.style.display = "none";
+    
+    productC.addEventListener("click", (event) => {
         event.preventDefault();
-        if (mostrarDescricao.style.display === "none") {
-            mostrarDescricao.style.display = "block";
-        } else {
-            mostrarDescricao.style.display = "none";
-        }
+        descricao.style.display = descricao.style.display === "none" ? "block" : "none";
     });
+});
